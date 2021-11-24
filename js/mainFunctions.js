81d7bf5c1dee16b8f0113252ac546e8cd0e4dd00
Yassir Khchaf <khchafyassirJS@gmail.com>
2021-11-24 19:26:39 +0100

    added show/hide password + more formatting

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -7,3 +7,22 @@ container.classList.add('right-panel-active'));
 
 signUpButton.addEventListener('click', () =>
 container.classList.remove('right-panel-active'));
+
+/*
+  function to manipulate show/hide password span
+*/
+function password_show_hide(i) {
+var x = document.getElementById("password-" + i);
+var show_eye = document.getElementById("show_eye-" + i);
+var hide_eye = document.getElementById("hide_eye-" + i);
+hide_eye.classList.remove("d-none");
+if (x.type === "password") {
+  x.type = "text";
+  show_eye.style.display = "none";
+  hide_eye.style.display = "block";
+} else {
+  x.type = "password";
+  show_eye.style.display = "block";
+  hide_eye.style.display = "none";
+}
+}
