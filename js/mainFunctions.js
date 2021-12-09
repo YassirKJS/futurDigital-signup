a4bb0ebf60cfb0adcf416c6e854874c7c8c486ec
Yassir Khchaf <khchafyassirJS@gmail.com>
2021-12-09 11:57:10 +0100

    finished design

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,3 +1,6 @@
+/*
+  Listeners to manage the panel class change
+*/
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -12,17 +15,22 @@ container.classList.remove('right-panel-active'));
   function to manipulate show/hide password span
 */
 function password_show_hide(i) {
-var x = document.getElementById("password-" + i);
-var show_eye = document.getElementById("show_eye-" + i);
-var hide_eye = document.getElementById("hide_eye-" + i);
-hide_eye.classList.remove("d-none");
-if (x.type === "password") {
-  x.type = "text";
-  show_eye.style.display = "none";
-  hide_eye.style.display = "block";
-} else {
-  x.type = "password";
-  show_eye.style.display = "block";
-  hide_eye.style.display = "none";
+  var x = document.getElementById("password-" + i);
+  var show_eye = document.getElementById("show_eye-" + i);
+  var hide_eye = document.getElementById("hide_eye-" + i);
+  hide_eye.classList.remove("d-none");
+  if (x.type === "password") {
+    x.type = "text";
+    show_eye.style.display = "none";
+    hide_eye.style.display = "block";
+  } else {
+    x.type = "password";
+    show_eye.style.display = "block";
+    hide_eye.style.display = "none";
+  }
 }
-}
+
+/*
+  Form Validation handling
+*/
+
