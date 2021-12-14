d8c8d3c6ac97a11647cb1407da6f3ce040171ed6
Yassir Khchaf <khchafyassirJS@gmail.com>
2021-12-14 10:55:24 +0100

    final commit

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -14,10 +14,10 @@ container.classList.remove('right-panel-active'));
 /*
   function to manipulate show/hide password span
 */
-function password_show_hide(i) {
-  var x = document.getElementById("password-" + i);
-  var show_eye = document.getElementById("show_eye-" + i);
-  var hide_eye = document.getElementById("hide_eye-" + i);
+let password_show_hide = (i) => {
+  let x = document.getElementById("password-" + i);
+  let show_eye = document.getElementById("show_eye-" + i);
+  let hide_eye = document.getElementById("hide_eye-" + i);
   hide_eye.classList.remove("d-none");
   if (x.type === "password") {
     x.type = "text";
@@ -34,29 +34,41 @@ function password_show_hide(i) {
   * Form Validation handling:
   *
   * id will target our id
-  * serial will target our classes [error class, success and failure icons]
+  * serial will target our error class
   * message will print a message inside our .error class 
   *
 */
 
 let form1 = document.getElementById("form1");
 let password1 = document.getElementById("password-1");
+let password2 = document.getElementById("password-2");
 let errorMsg = document.getElementsByClassName("error");
+const passReg = new RegExp("(?=.*?[#?!@$%^&*-])");
 
 form1.addEventListener("submit", (e) => {
   e.preventDefault();
-
   engine(username, 0, "Nom ne peut pas être vide");
   engine(email, 1, "Email ne peut pas être vide");
-  engine(password1, 2, "Mot de passe ne peut pas être vide");
+  engine(password1, 2, "Minimum 8 caractères dont 1 caractère spécial");
+  engine(password2, 3, "Mot de passe différent");
 });
 
 let engine = (id, serial, message) => {
-  if (id.value.trim() === "") {
+  if (id.value.trim() === "" && id != password2) {
     errorMsg[serial].innerHTML = message;
   } 
   
   else {
-    errorMsg[serial].innerHTML = "";
+    if (id == password1 && (password1.value.length <= 7 || !(passReg.test(password1.value)))) {
+      errorMsg[serial].innerHTML = message;
+    }
+    else {
+      if (id == password2 && password2.value != password1.value) {
+        errorMsg[serial].innerHTML = message;
+      }
+      else {
+        errorMsg[serial].innerHTML = "";
+      }      
+    }
   }
 }
